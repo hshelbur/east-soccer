ae248c4d4beba57e238fe49c9cb5dca34051410c
hshelbur <howard.shelburne@gmail.com>
2016-10-02 17:58:11 -0400

    add and remove player set state

--- roster.js
+++ roster.js
@@ -1,41 +1,3 @@
-const Button = props =>
-	<button onClick={props.onClick}  type="button" className="btn btn-primary" data-toggle="button" aria-pressed="false" autoComplete="off">
- 		{props.label}
-	</button>
-
-class PlayerProfile extends React.Component {
-	constructor () {
-		super();
-		this.state = {
-			selected: false
-		};
-		this.handleClick = this.handleClick.bind(this);
-	}
-	handleClick() {
-		this.setState({selected: !this.state.selected});
-	}	
-	render() {
-			
-			if(!this.state.selected) {
-				return <div className="well well-sm">						
-					<p className="text-center">{this.props.name}</p>
-					<img className="col-md-3" src="http://www.alvechurchlions.com/wp-content/uploads/2012/09/player_blank.jpeg" height="80" width="80" />
-					<p>Year: {this.props.year}</p>
-					<p>Position: {this.props.position}</p>
-					<Button label="Add Player" onClick={this.handleClick} />
-				</div>}
-			else{
-				return <div className="well well-sm">						
-					<p className="text-center">{this.props.name}</p>
-					<img className="col-md-3" src="http://www.alvechurchlions.com/wp-content/uploads/2012/09/player_blank.jpeg" height="80" width="80" />
-					<p>Year: {this.props.year}</p>
-					<p>Position: {this.props.position}</p>
-					<Button label="Remove Player" onClick={this.handleClick} />
-				</div>	
-				}
-	}
-}
-
 const Masthead = props =>
 	<div className="container fluid">
 		<div className="jumbotron">
@@ -43,26 +5,76 @@ const Masthead = props =>
 		</div>
 	</div>
 
+const Button = props =>
+	<button onClick={props.onClick}  type="button" className="btn btn-primary" data-toggle="button" aria-pressed="false" autoComplete="off">
+ 		{props.label}
+	</button>
 
-const PlayerLists = props =>
-	<div>
-		<div className="col-md-4">
-			<h4 className="text-center">Roster</h4>
-				
-			{props.players.map(function(player){
-				return <PlayerProfile key={player.id} name={player.name} year={player.year} position={player.position} />
-			})}
-		
-		</div>
-
-		<div className="col-md-4">
-			<h4 className="text-center">Selected Players</h4>
-		</div>
+const PlayerProfile = props =>
+	<div className="well well-sm">
+		<p className="text-center">{props.name}</p>		
+		<img className="col-md-3" src="http://www.alvechurchlions.com/wp-content/uploads/2012/09/player_blank.jpeg" height="80" width="80" />
+		<p>Year: {props.year}</p>
+		<p>Position: {props.position}</p>
+		<Button onClick={props.onClick} label={props.selected ? "Remove Player" : "Add Player"} />
 	</div>
+
+
+class PlayerLists extends React.Component {
+	constructor (){
+		super();
+		this.state = {
+			selected: [],
+			unselected: []
+		};
+		this.addPlayer = this.addPlayer.bind(this);
+		this.removePlayer = this.removePlayer.bind(this);
+	}
+
+	componentWillMount(){
+		this.setState({unselected: this.props.players})
+	}
+
+	addPlayer(player){
+		var selected = this.state.selected.concat(player);
+		var unselected = this.state.unselected.slice();
+		unselected.splice(unselected.indexOf(player), 1)
+		this.setState({selected, unselected})
+
+	}
+
+	removePlayer(player){
+		var unselected = this.state.unselected.concat(player);
+		var selected = this.state.selected.slice();
+		selected.splice(selected.indexOf(player), 1)
+		this.setState({unselected, selected})
+	}
+
+	render(){
+		return <div>
+			<div className="col-md-4">
+				<h4 className="text-center">Roster</h4>
+					
+				{this.state.unselected.map(player =>
+					<PlayerProfile onClick={() => this.addPlayer(player)} key={player.id} name={player.name} year={player.year} position={player.position} />
+				)}
+			
+			</div>
+
+			<div className="col-md-4">
+				<h4 className="text-center">Selected Players</h4>
+				{this.state.selected.map(player =>
+					<PlayerProfile onClick={() => this.removePlayer(player)} selected key={player.id} name={player.name} year={player.year} position={player.position} />
+				)}
+			</div>
+		</div>
+	}
+}
 
 const PLAYERS = [{id: "4", name: "Howard Shelburne", position: "Fucking Anywhere", year: "Pretty Damn Old"},
 				{id: "1", name: "Erin Turingan", position: "Cute Girl on the Field", year: "Forever Young"},
 				{id: "10", name: "Lionel Messi", position: "Dominance", year: "28"}]
+
 
 ReactDOM.render(
 	<div>
