e95ba94dd417160e349ab5be87c29ed12a37af4c
hshelbur <howard.shelburne@gmail.com>
2016-11-18 13:51:13 -0500

    moved src directory to assets directory and updated gulpfile accordingly

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ var gulp = require("gulp");
 var babel = require("gulp-babel");
 
 gulp.task("build-javascript", function () {
- return gulp.src('src/**/*.js')
+ return gulp.src('assets/**/*.js')
    .pipe(babel())
    .pipe(gulp.dest("dist"));
 });
 
 gulp.task('watch', function() {
-    gulp.watch('src/**/*', ['build-javascript'])
+    gulp.watch('assets/**/*', ['build-javascript'])
 }) 
 
 gulp.task('default', ['build-javascript'])
