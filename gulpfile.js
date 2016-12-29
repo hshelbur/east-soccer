08b5ae2c05e6b1ce0fd4727b7a606776a91c5091
hshelbur <howard.shelburne@gmail.com>
2016-12-29 14:33:55 -0500

    updating asset location and build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,26 @@
 var gulp = require("gulp");
 var babel = require("gulp-babel");
+var del = require('del');
+
+gulp.task('clean', function() {
+	return del.sync('public/*')
+})
 
 gulp.task("build-javascript", function () {
- return gulp.src('assets/**/*.js')
+ return gulp.src('assets/scripts/**/*.js')
    .pipe(babel())
-   .pipe(gulp.dest("dist"));
+   .pipe(gulp.dest("public/scripts"));
 });
 
+gulp.task("build-styles", function () {
+ return gulp.src('assets/styles/**/*.css')
+   .pipe(gulp.dest("public/styles"));
+});
+
+
 gulp.task('watch', function() {
-    gulp.watch('assets/**/*', ['build-javascript'])
+    gulp.watch('assets/**/*.js', ['build-javascript'])
+    gulp.watch('assets/**/*.css', ['build-styles'])
 }) 
 
-gulp.task('default', ['build-javascript'])
+gulp.task('default', ['clean', 'build-javascript', 'build-styles'])
