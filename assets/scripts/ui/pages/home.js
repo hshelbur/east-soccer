8289d7eaf568370617d93c4abd66a2da20903a59
hshelbur <howard.shelburne@gmail.com>
2017-01-02 18:03:41 -0500

    pulled reusable components out of page modules

--- assets/scripts/ui/pages/home.js
+++ assets/scripts/ui/pages/home.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Button from '../components/button'
+import Tabs from '../components/tabs'
 
 
 const WeatherUndergroundWidget = () =>
@@ -32,36 +34,6 @@ const Forum = () =>
 		</ul>
 	</div>
 	
-
-const Tabs = props => 
-	<div>
-		<ul className="nav nav-tabs" role="tablist">{props.children}</ul>
-
-			<div className="tab-content">
-
-			{React.Children.map(props.children, tab => {
-				var klass = "tab-pane active"
-				if(!tab.props.active){
-					klass = "tab-pane"
-				}
-				
-				return <div className={klass} id={tab.props.label.replace(/\s/g, '')}>{tab.props.children}</div>
-			})}
-		</div>
-	</div>
-
-const Button = props =>
-	<button onClick={props.onClick}  type="button" className="btn btn-primary" data-toggle="button" aria-pressed="false" autoComplete="off">
- 		{props.label}
-	</button>
-
-Tabs.Tab = props =>
-	<li className={props.active ? "active" : ""} ><a href={`#${props.label.replace(/\s/g, '')}`} role="tab" data-toggle="tab">{props.label}</a></li>
-
-Tabs.Tab.propTypes = {
-	active: React.PropTypes.bool,
-	label: React.PropTypes.string.isRequired,
-}
 
 class CreateContactForm extends React.Component {
 	constructor(props) {
--- assets/scripts/ui/pages/roster.js
+++ assets/scripts/ui/pages/roster.js
@@ -1,10 +1,6 @@
 import axios from 'axios'
 import React from 'react'
-
-const Button = props =>
-	<button onClick={props.onClick}  type="button" className="btn btn-primary" data-toggle="button" aria-pressed="false" autoComplete="off">
- 		{props.label}
-	</button>
+import Button from '../components/button'
 
 const PlayerProfile = props =>
 	<div className="well well-sm">
